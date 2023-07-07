9273a0bbe5bf7d5efc2b49108c45a4d6318cc98e
Leedonguk <donguk0105@naver.com>
2023-07-07 18:47:58 +0900

    BookingController 주소 수정

--- src/main/java/kopo/poly/Controller/BookingController.java
+++ src/main/java/kopo/poly/Controller/BookingController.java
@@ -1,6 +1,6 @@
 package kopo.poly.Controller;
 
-import kopo.poly.service.IBookingService;
+import kopo.poly.Service.IBookingService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
